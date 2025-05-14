94d65f147ba98aa2965da23d5bb02c2f5efd1ff5
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2025-05-14 12:34:40 +0200

    adjusted file locations

--- src/testofmatplotlib.py
+++ src/testofmatplotlib.py
@@ -14,12 +14,20 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib import font_manager
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 
 from matplotlib.ticker import FuncFormatter
 from scipy.stats import rayleigh, uniform, norm
 import mpltex
 
+font_dir = "fonts"  # adjust if needed
+
+# Add all .ttf or .TTF files
+for file in os.listdir(font_dir):
+    if file.lower().endswith(".ttf"):
+        font_path = os.path.join(font_dir, file)
+        font_manager.fontManager.addfont(font_path)
 
 def Lorentzian(x, center, FWHM, area, offset):
     '''
