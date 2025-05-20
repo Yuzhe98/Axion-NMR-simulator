4c84599a3475fdc4f7c77ff082607cd6a14dcdd1
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2025-05-20 20:52:22 +0200

    tested sqrt(tau_a T) and sqrt(tau_a T2) trend

---
+++ Tests/20250520-tau_a_《_T2/ALP-field-simu-iteration.py
@@ -0,0 +1,168 @@
+import os
+import sys
+
+from torch import dtype
+
+os.chdir("src")  # go to parent folder
+print(os.path.abspath(os.curdir))
+sys.path.insert(0, os.path.abspath(os.curdir))
+
+import pandas as pd
+import pickle
+
+import numpy as np
+import time
+from SimuTools import Sample, MagField, Simulation, gate
+from DataAnalysis import DualChanSig
+from functioncache import check, GiveDateandTime
+
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+num_runs = 1000
+simuRate = 500  #
+duration = 100
+timeLen = int(simuRate * duration)
+
+results = np.empty(
+    (num_runs, timeLen), dtype=np.float64
+)  # or float, depending on your data
+
+
+demodfreq = 1e6
+T2 = 30
+T1 = 1e9
+Brms = 1e-10
+nu_a = -0.7
+use_stoch = True
+
+savedir = rf"C:\Users\zhenf\D\Mainz\CASPEr\20250520-tau_a_《_T2/"
+timestr = GiveDateandTime()
+# # Create DataFrame with time and data columns
+# df = pd.DataFrame({"name": ["ALP field simulation"]})
+
+# # Store metadata in DataFrame attributes (won't save in CSV, but can save in pickle)
+# df.attrs["timeStamp"] = simu.timeStamp
+# df.attrs["m_transverse"] = [results]
+# df.attrs["simuRate"] = simuRate
+# df.attrs["duration"] = duration
+
+ExampleSample10MHzT = Sample(
+    name="TestSample",  # name of the atom/molecule
+    gyroratio=2
+    * np.pi
+    * (10)
+    * 1e6,  # [Hz/T]. Remember input it like 2 * np.pi * 11.777*10**6
+    T2=T2,  # [s]
+    T1=T1,  # [s]
+    pol=1,
+    verbose=False,
+)
+
+ALP_Field_grad = MagField(
+    name="ALP field gradient"
+)  # excitation field in the rotating frame
+# excField.nu = 1e6 - 10  # [Hz]
+
+simu = Simulation(
+    name="TestSample 10MHzT",
+    sample=ExampleSample10MHzT,  # class Sample
+    # gyroratio=(2*np.pi)*11.777*10**6,  # [Hz/T]
+    init_time=0.0,  # [s]
+    station=None,
+    init_mag_amp=1.0,
+    init_M_theta=0.0,  # [rad]
+    init_M_phi=0.0,  # [rad]
+    demodfreq=demodfreq,
+    B0z=(1e6) / (ExampleSample10MHzT.gyroratio / (2 * np.pi)),  # [T]
+    simuRate=simuRate,  #
+    duration=duration,
+    excField=ALP_Field_grad,
+    verbose=False,
+)
+
+
+for i in range(num_runs):
+    rand_seed = i
+
+    # tic = time.perf_counter()
+    # check(simu.demodfreq)
+    simu.excField.setALP_Field(
+        method="inverse-FFT",
+        timeStamp=simu.timeStamp,
+        Brms=Brms,  # RMS amplitude of the pseudo-magnetic field in [T]
+        nu_a=nu_a,  # frequency in the rotating frame
+        # direction: np.ndarray,  #  = np.array([1, 0, 0])
+        use_stoch=use_stoch,
+        demodfreq=simu.demodfreq,
+        # rand_seed=rand_seed,
+        makeplot=False,
+    )
+    simu.excType = "ALP"
+    # toc = time.perf_counter()
+    # print(f"setALP_Field() time consumption = {toc-tic:.3f} s")
+
+    # tic = time.perf_counter()
+    simu.GenerateTrajectory(verbose=False)
+    # toc = time.perf_counter()
+    # print(f"GenerateTrajectory time consumption = {toc-tic:.3f} s")
+
+    # simu.MonitorTrajectory(verbose=True)
+    # simu.VisualizeTrajectory3D(
+    #     plotrate=1e3,  # [Hz]
+    #     # rotframe=True,
+    #     verbose=False,
+    # )
+    # Delta_nu_a = 1.2  # Hz
+    # tau_a = 1 / (np.pi * Delta_nu_a)
+    # T2 = simu.sample.T2
+    # tau = np.sqrt(tau_a * T2)
+    # # check(tau)
+    # check(1 / (np.pi * T2))
+    # check(1 / (np.pi * tau))
+    # check(1 / (tau))
+    # simu.compareBandSig()
+
+    # normalized magnetization
+    m_t = np.sqrt(simu.trjry[0:-1, 0] ** 2 + simu.trjry[0:-1, 1] ** 2)
+
+    # m_transverse = run_simulation(rand_seed)
+    results[i] = m_t
+    # np.save(f"m_transverse_run_{i}.npy", m_transverse)
+
+
+# comment = f"Transverse magnetization. simuRate: {simuRate}"
+data_file_name = savedir + f"m_transverse_all_runs_" + timestr + ".npz"
+np.savez(
+    data_file_name,
+    timeStamp=simu.timeStamp,
+    m_t=results,
+    simuRate=simuRate,
+    duration=duration,
+    demodfreq=demodfreq,
+    T2=T2,
+    T1=T1,
+    Brms=Brms,
+    nu_a=nu_a,
+    use_stoch=True,
+    gyroratio=ExampleSample10MHzT.gyroratio,
+)
+
+
+# np.save(savedir + f"m_transverse_all_runs_" + timestr + ".npy", results)
+
+
+# # Sample data
+# m_transverse = np.random.rand(1000)
+# simuRate = 1000
+# duration = 1.0
+
+
+# # Save DataFrame as pickle (preserves attrs)
+
+# data_file_name = savedir + f"m_transverse_all_runs_" + timestr + ".pkl"
+# df.to_pickle(data_file_name)
+
+# with open(data_file_name, "wb") as f:
+#     pickle.dump({"df": df, "m_transverse": results}, f)
---
+++ Tests/20250520-tau_a_《_T2/data-analysis.py
@@ -0,0 +1,183 @@
+import os
+import sys
+
+os.chdir("src")  # 
+print(os.path.abspath(os.curdir))
+sys.path.insert(0, os.path.abspath(os.curdir))
+os.chdir("..")  # go to parent folder
+
+import pandas as pd
+import numpy as np
+import time
+from SimuTools import Sample, MagField, Simulation, gate
+from DataAnalysis import DualChanSig
+from functioncache import check, GiveDateandTime
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+import numpy as np
+
+# Load the .npy file
+# savedir = rf"C:\Users\zhenf\D\Mainz\CASPEr\20250520-tau_a_《_T2/"
+savedir = rf"Tests\20250520-tau_a_《_T2/"
+# fname = "m_transverse_all_runs_20250520_145634"  # rand_seed not None, 300s duration, Brms=1e-10
+# fname = "m_transverse_all_runs_20250520_151127"  # rand_seed not None, 100s duration, Brms=1e-10
+# fname = "m_transverse_all_runs_20250520_151424"  # rand_seed None, 100s duration, Brms=1e-10
+fname = "m_transverse_all_runs_20250520_153449"
+data = np.load(savedir + fname + ".npz")
+# plotRate = 1000
+
+# data = np.load("sim_data.npz", allow_pickle=True)
+
+print(data["m_t"].shape)  # (1000,)
+print(data["simuRate"])  # 1000
+print(data["duration"])  # 1.0
+print(data["T2"])  # 1.0
+print(data["Brms"])  # 1.0
+
+m_t_avg = np.mean(data["m_t"], axis=0)
+check(m_t_avg.shape)
+
+m_t_std = np.std(data["m_t"], axis=0)
+check(m_t_std.shape)
+
+fig = plt.figure(figsize=(6.0, 4.0), dpi=150)  # initialize a figure
+gs = gridspec.GridSpec(nrows=1, ncols=1)  # create grid for multiple figures
+# #############################################################################
+# to specify heights and widths of subfigures
+# width_ratios = [1, 1]
+# height_ratios = [1]
+# gs = gridspec.GridSpec(nrows=1, ncols=2, \
+#   width_ratios=width_ratios, height_ratios=height_ratios)  # create grid for multiple figures
+# #############################################################################
+# # fix the margins
+# left=0.171
+# bottom=0.202
+# right=0.952
+# top=0.983
+# wspace=0.24
+# hspace=0.114
+# fig.subplots_adjust(left=left, top=top, right=right,
+#                     bottom=bottom, wspace=wspace, hspace=hspace)
+# #############################################################################
+# plotintv = len(data["timeStamp"] / )
+ax00 = fig.add_subplot(gs[0, 0])
+# ax00.errorbar(
+#     x=data["timeStamp"], y=m_t, yerr=m_t_std, label=""
+# )
+ax00.plot(data["timeStamp"], m_t_avg, label="")
+# ax00.plot(x=data["timeStamp"], y=m_t-m_t_std, label="")
+# ax00.plot(x=data["timeStamp"], y=m_t+m_t_std, label="")
+
+
+# ax00.fill_between(
+#     data["timeStamp"],
+#     m_t - m_t_std,
+#     m_t + m_t_std,  #  where = ,
+#     color="r",
+#     alpha=0.2,
+#     zorder=6,
+# )
+print(data["gyroratio"] * data["Brms"])
+check(data["gyroratio"] * data["Brms"])
+
+time_sections = [[0, data["T2"] / 10],  [data["T2"] / 3,data["T2"]]]
+T_2_idx = int(len(data["timeStamp"]) * data["T2"] / data["duration"])
+time_sections_idx = [[0, T_2_idx // 10], [T_2_idx // 10, T_2_idx //2]]
+
+time_section0 = data["timeStamp"][time_sections_idx[0][0]: time_sections_idx[0][1]]
+approx_line0 = data["gyroratio"] * data["Brms"] * time_section0
+approx_line0 /= np.mean(approx_line0)
+approx_line0 *= np.mean(m_t_avg[time_sections_idx[0][0] : time_sections_idx[0][1]])
+ax00.plot(time_section0, approx_line0, label="")
+
+time_section1 = data["timeStamp"][time_sections_idx[1][0]: time_sections_idx[1][1]]
+approx_line1 = data["gyroratio"] * data["Brms"] * np.sqrt(time_section1 * 1. /(1.2 * np.pi ))
+approx_line1 /= np.mean(approx_line1)
+approx_line1 *= np.mean(m_t_avg[time_sections_idx[1][0] : time_sections_idx[1][1]])
+ax00.plot(time_section1, approx_line1, label="")
+# check(approx_line0[0:10])
+
+
+ymin, ymax = ax00.get_ylim()
+
+
+# ax00.plot( data["m_t"], label="")
+ax00.set_xlabel("")
+ax00.set_ylabel("")
+# ax00.set_xscale('log')
+# ax00.set_yscale('log')
+
+# ax00.vlines([data["T2"] / 10, data["T2"]], ymin=ymin, ymax=ymax, linestyles='dashed')
+
+
+# Vertical lines and their labels
+vline_positions = [data["T2"] / 10, data["T2"]]
+vline_labels = ["$T_2 / 10$", "$T_2$"]
+# Draw vertical lines with labels
+for xpos, label in zip(vline_positions, vline_labels):
+    ax00.axvline(x=xpos, color="k", linestyle="--", alpha=1)
+    ax00.text(
+        xpos,
+        1.05,
+        label,
+        rotation=90,
+        verticalalignment="bottom",
+        horizontalalignment="center",
+        color="k",
+    )
+
+# ax00.legend()
+# #############################################################################
+# fig.suptitle("", wrap=True)
+# #############################################################################
+# # put figure index
+# letters = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)", "(g)", "(h)", "(i)"]
+# for i, ax in enumerate([ax00, ax10]):
+#     xleft, xright = ax00.get_xlim()
+#     ybottom, ytop = ax00.get_ylim()
+#     ax.text(x=xleft, y=ytop, s=letters[i], ha="right", va="bottom", color="blue")
+# # ha = 'left' or 'right'
+# # va = 'top' or 'bottom'
+# #############################################################################
+# # put a mark of script information on the figure
+# # Get the script name and path automatically
+# script_path = os.path.abspath(__file__)
+# # Add the annotation to the figure
+# plt.annotate(
+#     f"Generated by: {script_path}",
+#     xy=(0.02, 0.02),
+#     xycoords="figure fraction",
+#     fontsize=3,
+#     color="gray",
+# )
+# #############################################################################
+plt.tight_layout()
+# plt.savefig('example figure - one-column.png', transparent=False)
+plt.show()
+
+
+# print("Shape:", data.shape)  # e.g., (100, 1000) → 100 runs, 1000 time points each
+# print("Dtype:", data.dtype)  # e.g., complex128, float64, etc.
+# print("First few values:\n", data[0][:10])  # Preview first 10 values from the first run
+# import matplotlib.pyplot as plt
+
+
+# Load it back
+
+# df = pd.read_pickle(savedir + f"m_transverse_all_runs_" + timestr + ".pkl")
+# # print("time:", df.attrs["time"][0:10])
+# print("result:", df.attrs["result"][0:10])
+# print("simuRate:", df.attrs["simuRate"])
+# print("duration:", df.attrs["duration"])
+# print(df.head())
+
+
+# plt.plot(np.abs(data[0]))  # magnitude of transverse magnetization
+# plt.title("Run 0: Magnitude of Transverse Magnetization")
+# plt.xlabel("Time")
+# plt.ylabel("|M_transverse|")
+# plt.grid(True)
+# plt.show()
